fd92f83fe18ffb7300971af1d978132a171bb82d
Stuti Gaonkar <stuti@Stutis-MacBook-Pro.local>
2026-01-31 12:32:14 -0800

    Edited API keys

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import plotly.graph_objects as go
 import requests
 from dotenv import load_dotenv
 
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY") or os.getenv("OPENAI_API_KEY")
+
 load_dotenv()
 
 # ---------------------------
